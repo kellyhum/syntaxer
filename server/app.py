8458751d97f2e12e3352b8a2e5dfcce240b9abf1
Kelly Hum <kellyhum88@gmail.com>
2024-12-31 22:09:17 -0800

    test out openai api call

--- server/app.py
+++ server/app.py
@@ -1,12 +1,29 @@
 from flask import Flask
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    api_key= os.getenv('OPENAI_API_KEY')
+)
 
 app = Flask(__name__) # setup paths
 CORS(app) # enable cors on all routes
 
 @app.route('/msg')
 def test():
-    return 'hello world! backend up and running'
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "write a haiku about ai"}
+        ]
+    )
+
+    return completion.choices[0].message
+    #return 'hello world! backend up and running'
 
 if __name__ == '__main__':
     app.run(debug = True)
