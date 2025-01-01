09e4ba116445c1305fb32f77fb7f589fb5272605
Kelly Hum <kellyhum88@gmail.com>
2024-12-31 22:22:01 -0800

    remove openai api calls, choose a random prompt from the array

--- server/app.py
+++ server/app.py
@@ -1,29 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-import os
-from dotenv import load_dotenv
-from openai import OpenAI
-
-load_dotenv()
-
-client = OpenAI(
-    api_key= os.getenv('OPENAI_API_KEY')
-)
+import random
+from writingPrompts import writing_prompts
 
 app = Flask(__name__) # setup paths
 CORS(app) # enable cors on all routes
 
 @app.route('/msg')
-def test():
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "user", "content": "write a haiku about ai"}
-        ]
-    )
-
-    return completion.choices[0].message
-    #return 'hello world! backend up and running'
+def getWritingPrompt():
+    return random.choice(writing_prompts['A1'])
 
 if __name__ == '__main__':
     app.run(debug = True)
