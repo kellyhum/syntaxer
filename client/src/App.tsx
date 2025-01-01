e695f8137820eba644aa1642a9fbdcfdc69a68e6
Kelly Hum <kellyhum88@gmail.com>
2024-12-31 22:09:35 -0800

    add select button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
-import { Flex, Box, Text, Heading, Textarea, Button } from "@chakra-ui/react";
+import {
+    Flex,
+    Box,
+    Text,
+    Heading,
+    Textarea,
+    Button,
+    createListCollection,
+    ListCollection,
+} from "@chakra-ui/react";
+import {
+    SelectContent,
+    SelectItem,
+    SelectRoot,
+    SelectTrigger,
+    SelectValueText,
+} from "@/components/ui/select";
 import { useEffect, useState } from "react";
 
 export default function App() {
+    const levels: ListCollection<string> = createListCollection({
+        items: ["A1", "A2", "B1", "B2"],
+    });
     const [serverMsg, setServerMsg] = useState(""); // holds the msg value from backend (datatype: string)
 
     useEffect(() => {
+        // only works if you start flask first -> otherwise will give network error
         fetch("http://127.0.0.1:5000/msg")
             .then((response) => response.text())
             .then((data) => setServerMsg(data));
@@ -13,17 +33,37 @@ export default function App() {
     return (
         <Box px={100} py={50} height="100%">
             <Flex justifyContent="space-between" align="center">
-                <Heading>language paragraph practice tool</Heading>
+                <Heading mb={10}>language paragraph practice tool</Heading>
                 {/* <CustomButton title="Login" /> */}
             </Flex>
 
-            <Text mt={15} mb={10}>
-                Random paragraph writing prompt here
-            </Text>
-
-            <Text>0:00 (timer)</Text>
-
             <form>
+                <Flex justifyContent="space-between">
+                    <Text mt={15} mb={10}>
+                        Random paragraph writing prompt here
+                    </Text>
+
+                    <Flex alignItems="center" gap={5}>
+                        <Text>0:00 (timer)</Text>
+
+                        <SelectRoot collection={levels} width="12rem">
+                            <SelectTrigger>
+                                <SelectValueText placeholder="Select CEFR level" />
+                            </SelectTrigger>
+                            <SelectContent
+                                backgroundColor="white"
+                                _hover={{ bg: "white" }}
+                            >
+                                {levels.items.map((level) => (
+                                    <SelectItem item={level}>
+                                        {level}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </SelectRoot>
+                    </Flex>
+                </Flex>
+
                 <Textarea
                     name="userParagraph"
                     placeholder="Type your paragraph here..."
